37ec64548f635a71a11af359a001efe0b73811e1
JoseFlores <joseolmedo.florespaniagua@triosstudent.com>
2026-04-22 12:10:58 -0400

    Add basic global styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 
 
 import Link from "next/link";
+import "./globals.css";
 
 export default function RootLayout({
   children,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,10 @@
 
-import Link from "next/link"
 
 export default function Home() {
   return (
     <div>
       <h1>My Portfolio</h1>
       <p>Welcome to my website.</p>
-
-      <nav>
-        <Link href="/">Home</Link> |{" "} 
-        <Link href="/about">About</Link> |{" "}
-        <Link href="/projects">Projects</Link> |{" "}
-        <Link href="/contact">Contact</Link>
-      </nav>
     </div>
   );
 }
